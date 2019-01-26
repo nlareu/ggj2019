72b19cf38d6d59bce4488f81804b98eb4cc5c021
nlareu2@gmail.com <nlareu2@gmail.com>
2019-01-26 15:47:20 -0300

    addd menu and rules

---
+++ SourceCode/ggj2019/Assets/Scripts/MenuController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour {
+
+    public string NextSceneName;
+
+    // Use this for initialization
+    void Start () {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        //Wait for any key input to start the game play.
+        if (Input.anyKey)
+        {
+            //this.audioSource_backgroundMusic.Stop();
+
+            SceneManager.LoadScene(this.NextSceneName);
+        }
+    }
+}
